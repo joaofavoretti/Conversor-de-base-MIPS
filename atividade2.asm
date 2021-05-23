fd99c0a34610572a03daa909352518cb375bd084
Joao Pedro Favoretti <joaopedrofavoretti@gmail.com>
2021-05-23 19:55:43 -0300

    Funcionalidade decimal -> hex

--- atividade2.asm
+++ atividade2.asm
@@ -1,8 +1,8 @@
 # Atividade 2 - Organizacao e Arquitetura de Computadores
 # 
 # Nomes dos alunos:
-# João Pedro Favoretti (11316055)
-# Lucas Pilla ()
+# João Pedro Favoretti (Nusp: 11316055)
+# Lucas Pilla Pimentel (Nusp: 10633328)
 # ====================
 # TABELA DE PROCEDIMENTOS:
 # 
@@ -19,43 +19,44 @@
 
 ## SEGMENTO DE DATA
 .data
-	.align 	2			# Alinhar variaveis à word
-	entrada_dec:	.word 0		# Variavel para entrada de numero decimal
-	entrada_hex:	.space 16	# Variavel para entrada de numero hexadecimal
-	entrada_bin:	.space 64	# Variavel para entrada de numero binario
-	base_entrada:	.space 2	# Espaco para armazenar a base da entrada
+	.align 	2				# Alinhar variaveis à word
+	entrada_dec:	.word 0			# Variavel para entrada de numero decimal
+	entrada_hex:	.asciiz "         "	# Variavel para entrada de numero hexadecimal
+	entrada_bin:	.space 64		# Variavel para entrada de numero binario
+	base_entrada:	.space 2		# Espaco para armazenar a base da entrada
 	
-	saida_dec:	.word 0		# Variavel para saida de numero decimal
-	saida_hex:	.space 16	# Variavel para saida de numero hexadecimal
-	saida_bin:	.space 64	# Variavel para saida de numero binario
-	base_saida:	.space 2	# Espaco para armazenar a base da saida
+	saida_dec:	.word 0			# Variavel para saida de numero decimal
+	saida_hex:	.asciiz "         "	# Variavel para saida de numero hexadecimal
+	saida_bin:	.space 64		# Variavel para saida de numero binario
+	base_saida:	.space 2		# Espaco para armazenar a base da saida
 	
+	hexa_char:	.asciiz "0123456789abcdef"	# Caracteres Hexadecimais
 	pulo_linha:	.asciiz	"\n"
 
 ## SEGMENTO DE CÓDIGO
 .text
 .globl main
 main:
-	# Read binary
-	li 	$v0, 8
-	la	$a0, entrada_bin
-	li	$a1, 32
+	li 	$v0, 5
 	syscall
 	
-	# binary to decimal
-	la	$a0, entrada_bin
-	la	$a1, saida_dec
-	jal 	binaryToDecimal
+	la	$t0, entrada_dec
+	sw	$v0, 0($t0)
+	
+	la	$a0, entrada_dec
+	la	$a1, saida_hex
+	la	$a2, hexa_char
+	
+	jal decimalToHexadecimal
 	
 	# Print "\n"
 	li	$v0, 4
 	la	$a0, pulo_linha
 	syscall
 	
-	# Print integer
-	la	$t0, saida_dec
-	lw	$a0, 0($t0)
-	li 	$v0, 1
+	# Print Hexa string
+	li	$v0, 4
+	la	$a0, saida_hex
 	syscall
 	
 	# Exit
@@ -116,6 +117,7 @@ potencia:				## POTENCIA PROCEDURE ##
 binaryToDecimal:			## BINARY TO DECIMAL PROCEDURE ##
 					# $a0: endereco para .space 33 bytes (binario)
 					# $a1: endereco para .word (decimal)
+					# 
 					# return void
 					#
 					# Based code
@@ -197,8 +199,84 @@ binaryToDecimal:			## BINARY TO DECIMAL PROCEDURE ##
 	addi	$sp, $sp, 16		# Dealloc Stack
 	
 	jr	$ra
+
+
+decimalToHexadecimal:			## DECIMAL TO HEXADECIMAL PROCEDURE ##
+					# $a0: endereco para .word (decimal)
+					# $a1: endereco para .space 9 bytes (binario)
+					# $a2: endereco para .asciiz com caracteres hexadecimais
+					# return void
+					#
+					# Based code
+					# void decimalToHexa(int *dec, char *hexa, char *charHexa) {
+					#     int i, j, k;
+					#     for (j = 8, i = *dec; i != 0; i = i / 16, j--) {
+					#         k = i % 16;
+					#         hexa[j] = charHexa[k];
+					#     }
+					# }
+					# 
+					# STACK
+					# +-----------+
+					# | $a0(&dec) | + 20
+					# | $a1(&hex) | + 16
+					# | $a2(&hxc) | + 12
+					# | i         | + 8
+					# | j         | + 4
+					# | k         | + 0
+					# +-----------+ $sp
 	
+	addi	$sp, $sp, -24		# Alloc stack
 	
+	sw	$a0, 20($sp)		# Save arguments
+	sw	$a1, 16($sp)		# Save arguments
+	sw	$a2, 12($sp)		# Save arguments
+	
+	li 	$t0, 8			# j = 8
+	sw	$t0, 4($sp)		# j = 8
+
+	lw	$t0, 20($sp)		# $t0 (&dec)
+	lw	$t1, 0($t0)		# $t1 (dec)
+	sw	$t1, 8($sp)		# i = dec
+	
+	L4:
+	lw	$t0, 8($sp)		# $to (i)
+	beq	$t0, $zero, E_L4	# if i == 0 then E_L4
+	
+	lw	$t0, 8($sp)		# $t0 (i)
+	rem 	$t1, $t0, 16		# k = i % 16
+	sw	$t1, 0($sp)		# k = i % 16
+	
+	lw	$t0, 16($sp)		# $t0 (&hex)
+	lw	$t1, 4($sp)		# $t1 (j)
+	addu	$t0, $t0, $t1		# $t0 (&hex[i])
+	
+	lw	$t1, 12($sp)		# $t1 (&hxc)
+	lw	$t2, 0($sp)		# $t2 (k)
+	addu	$t1, $t1, $t2		# $t1 (&hxc[k])
+	
+	lb	$t2, 0($t1)		# hex[i] = hxc[k]
+	sb	$t2, 0($t0)		# hex[i] = hxc[k]
+	
+	lw	$t0, 8($sp)		# $t0 (i)
+	div	$t0, $t0, 16		# i = i / 16
+	sw	$t0, 8($sp)		# i = i / 16
+	
+	lw	$t0, 4($sp)		# $t0 (j)
+	addi	$t0, $t0, -1		# j = j - 1
+	sw	$t0, 4($sp)		# j = j - 1
+	
+	j 	L4			# jump to L4
+	
+	E_L4:
+	
+	lw	$a0, 20($sp)		# Load arguments
+	lw	$a1, 16($sp)		# Load arguments
+	lw	$a2, 12($sp)		# Load arguments
+
+	addi	$sp, $sp, 24		# Dealloc stack
+	
+	jr $ra
 	
 		
 
